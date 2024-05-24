f0033f95d35ceeadfb7b308afaa7a5aaafc8154a
khansrfrz <khansarfaraz205@gmail.com>
2024-05-24 23:48:37 +0530

    v1.0

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './index.css'
+
+function App() {
+
+
+  return (
+    <nav className=' bg-gray-800 p-4'>
+     <div className='bg-gray-800 p-4 container mx-auto flex justify-between items-center '>
+       <div className='text-white text-lg font-bold'>
+        <h1 className=" mx-auto  text-sky-300  font-bold  justify-center items-center  left-2/4 right-2/4">KARISHMA TIWARI</h1>
+    
+       </div>
+
+    
+     </div>
+    
+    </nav>
+  )
+}
+
+export default App
