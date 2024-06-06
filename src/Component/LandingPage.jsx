390f504aa4a293a286fb75acd7c475d8ac2979e2
khansrfrz <khansarfaraz205@gmail.com>
2024-06-06 12:30:22 +0530

    xxxx

--- src/Component/LandingPage.jsx
+++ src/Component/LandingPage.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { HiCode } from "react-icons/hi";
 import { Link } from "react-router-dom";
-import resumeImage from "../assets/Resume.pdf"
-import coverImage from "../assets/pfp.png"
+import resumeImage from "../assets/Resume.pdf";
+import coverImage from "../assets/pfp.png";
 
 const LandingPage = () => {
   return (
@@ -15,26 +15,80 @@ const LandingPage = () => {
           </div>
           <nav>
             <div className="flex space-x-4">
-            <Link to="/home" className="text-gray-700 hover:text-blue-500 font-bold ">Home</Link>
-           <Link to="/about" className="text-gray-700 hover:text-blue-500 font-bold">About</Link>
-           <Link to="/services" className="text-gray-700 hover:text-blue-500 font-bold">Services</Link>
-           <Link to="/Pricing" className="text-gray-700 hover:text-blue-500 font-bold">Pricing</Link>
-           <Link to="/Contact" className="text-gray-700 hover:text-blue-500 font-bold">Contact</Link>
-           <Link to={resumeImage}  alt="Profile" className="hidden md:inline-block bg-blue-500 text-white px-4 py-1 rounded-md">Download CV</Link> 
+              <Link
+                to="/home"
+                className="text-gray-700 hover:text-blue-500 font-bold  px-4"
+              >
+                Home
+              </Link>
+              <Link
+                to="/about"
+                className="text-gray-700 hover:text-blue-500 font-bold px-4"
+              >
+                About
+              </Link>
+              <Link
+                to="/services"
+                className="text-gray-700 hover:text-blue-500 font-bold px-4"
+              >
+                Services
+              </Link>
+              <Link
+                to="/Pricing"
+                className="text-gray-700 hover:text-blue-500 font-bold px-4"
+              >
+                Pricing
+              </Link>
+              <Link
+                to="/Contact"
+                className="text-gray-700 hover:text-blue-500 font-bold px-4"
+              >
+                Contact
+              </Link>
+              <Link
+                to={resumeImage}
+                alt="Profile"
+                className="hidden md:inline-block bg-blue-500 text-white px-4 rounded-md"
+              >
+                Download CV
+              </Link>
             </div>
-           </nav>
+          </nav>
         </div>
       </header>
       <main className=" conatainer mx-auto px-4 py-20">
         <div className=" flex flex-col md:flex-row items-center">
           <div className="md:w-1/2 mb-8 md:mb-0">
-            <p className=" text-blue-500">Hey There<span role="img" aria-label="waving hand">👋</span> I am </p>
+            <p className=" text-blue-500">
+              Hey There
+              <span role="img" aria-label="waving hand">
+                👋
+              </span>{" "}
+              I am{" "}
+            </p>
             <h1 className="text-4xl font-bold mb-2">Sarfaraz Khan</h1>
-            <h2 className=" text-gray-600 mb-4">Professional Frontend Developer</h2>
-            <p className="text-gray-600 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptate, aut corrupti quos voluptates nam quam nesciunt pariatur eius delectus id, vero eum quod dolore? Eaque aut a consequuntur eligendi.</p>
+            <h2 className=" text-gray-600 mb-4">
+              Professional Frontend Developer
+            </h2>
+            <p className="text-gray-600 mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+              voluptate, aut corrupti quos voluptates nam quam nesciunt pariatur
+              eius delectus id, vero eum quod dolore? Eaque aut a consequuntur
+              eligendi.
+            </p>
             <div className="flex space-x-4">
-              <a href="#" className="bg-blue-500 text-white px-4 py-2 rounded-md">Contact Me</a>
-              <a href="#" className="bg-gray-900 text-white px-4 py-2 rounded-md">Learn More</a>
+              <a
+                href="#"
+                className="bg-blue-500 text-white px-4 py-2 rounded-md"
+              >
+                Contact Me
+              </a>
+              <a
+                href="#"
+                className="bg-gray-900 text-white px-4 py-2 rounded-md"
+              >
+                Learn More
+              </a>
             </div>
           </div>
           <div className="md:w-1/2 flex justify-center">
@@ -45,6 +99,5 @@ const LandingPage = () => {
     </div>
   );
 };
-          
 
 export default LandingPage;
